e315725eef54153e60a42873b9cae408607bf124
Ndivhu4Coding <st10385758@vcconnect.edu.za>
2024-03-06 11:55:22 +0200

    ICE task

---
+++ ConsoleStopSign/src/consolestopsign/ConsoleStopSign.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package consolestopsign;
+
+public class ConsoleStopSign {
+    public static void main(String[] args) {
+        // Print the stop sign using ASCII art
+        
+        // Print the stop sign using stars
+        System.out.println("   *****");
+        System.out.println("  *     *");
+        System.out.println(" *       *");
+        System.out.println("*  STOP   *");
+        System.out.println(" *       *");
+        System.out.println("  *     *");
+        System.out.println("   *****");    
+        System.out.println("    **     "); 
+        System.out.println("    **     ");
+        System.out.println("    **     "); 
+        System.out.println("    **     "); 
+        System.out.println("***********");
+
+
+    }
+}
